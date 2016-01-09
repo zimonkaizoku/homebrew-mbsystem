47db5e3eae3077558a64bd052ca1a4c5ef9dfbb4
zimonkaizoku <zimon.kaizoku@gmail.com>
2016-01-09 17:24:01 +0100

    fixes for new build system

--- mbsystem.rb
+++ mbsystem.rb
@@ -5,12 +5,13 @@ class Mbsystem < Formula
 
   depends_on :x11
   depends_on "gmt"
+  depends_on "gdal"
   depends_on "netcdf"
   depends_on "proj"
   depends_on "fftw"
   depends_on "homebrew/x11/gv"
   depends_on "lesstif"
-  # depends_on "zimonkaizoku/mbsystem/openmotif"
+  #depends_on "zimonkaizoku/mbsystem/openmotif"
   depends_on "zimonkaizoku/mbsystem/otps" => :recommended
 
   option "without-levitus", "Don't install Levitus database (no mblevitus)"
@@ -33,20 +34,29 @@ class Mbsystem < Formula
       "--prefix=#{prefix}",
       "--disable-static",
       "--enable-shared",
-      "--with-netcdf-include=#{Formula["netcdf"].opt_include}",
       "--with-netcdf-lib=#{Formula["netcdf"].opt_lib}",
-      "--with-gmt-include=#{Formula["gmt"].opt_include}/gmt",
+      "--with-netcdf-include=#{Formula["netcdf"].opt_include}",
+      "--with-gdal-lib=#{Formula["gdal"].opt_lib}",
+      "--with-gdal-include=#{Formula["gdal"].opt_include}",
       "--with-gmt-lib=#{Formula["gmt"].opt_lib}",
-      "--with-proj-include=#{Formula["proj"].opt_include}",
+      "--with-gmt-include=#{Formula["gmt"].opt_include}/gmt",
       "--with-proj-lib=#{Formula["proj"].opt_lib}",
-      "--with-fftw-include=#{Formula["fftw"].opt_include}",
+      "--with-proj-include=#{Formula["proj"].opt_include}",
       "--with-fftw-lib=#{Formula["fftw"].opt_lib}",
-      "--with-motif-include=#{Formula["lesstif"].opt_include}",
-      "--with-motif-lib=#{Formula["lesstif"].opt_lib}"      
-      # "--with-motif-include=#{Formula["openmotif"].opt_include}",
-      # "--with-motif-lib=#{Formula["openmotif"].opt_lib}"
+      "--with-fftw-include=#{Formula["fftw"].opt_include}",
+      "--with-motif-lib=#{Formula["lesstif"].opt_lib}",
+      "--with-motif-include=#{Formula["lesstif"].opt_include}"
+      #"--with-motif-lib=#{Formula["openmotif"].opt_lib}",
+      #"--with-motif-include=#{Formula["openmotif"].opt_include}"
+
+      #"--with-netcdf-config=#{Formula["netcdf"].opt_lib}/pkgconfig/netcdf.pc"
+      #"--with-gdal-config=#{Formula["gdal"].opt_include}/cpl_config.h"
+      #"--with-gmt-config=#{Formula["gmt"].opt_include}/gmt/config.h"
+
     ]
     args << "--with-otps-dir=#{Formula["otps"].prefix}" if build.with? "otps"
+
+    ENV['CFLAGS']="-I/opt/X11/include -L/opt/X11/lib"
 
     system "./configure", *args
     system "make", "check" if build.with? "check"
@@ -60,6 +70,12 @@ class Mbsystem < Formula
       on this computer that want to use mbsystem. Run the
       following command within the home directory:
           gmtset GMT_CUSTOM_LIBS #{HOMEBREW_PREFIX}/lib/libmbgmt.dylib
+
+      Additionally, if not already done within the gmt
+      installation, the directories for DCW and GSHHG (borders,
+      coast lines, rivers, etc.) need to be set:
+          gmtset DIR_DCW #{HOMEBREW_PREFIX}/share/gmt/dcw
+          gmtset DIR_GSHHG #{HOMEBREW_PREFIX}/share/gmt/coast
 
     EOS
   end
