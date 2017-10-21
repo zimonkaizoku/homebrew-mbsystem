61102bdbb7ec2c01a9d5d609382104950f34fa1b
dwcaress <caress@mbari.org>
2017-10-21 15:00:23 -0700

    Added conflicts_with symbol

--- mbsystem.rb
+++ mbsystem.rb
@@ -13,6 +13,8 @@ class Mbsystem < Formula
   depends_on "gv"
   depends_on "openmotif"
   depends_on "dwcaress/mbsystem/otps"
+  
+  conflicts_with "mbsystem-beta", :because => "mbsystem and mbsystem-beta share the same commands“
 
   option "without-check", "Disable build time checks (not recommended)"
 
