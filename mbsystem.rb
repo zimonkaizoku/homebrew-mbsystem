a8572b099a183f60f5f3b06faff25f6da3ef69ea
dwcaress <caress@mbari.org>
2017-10-23 17:41:06 -0700

    Update mbsystem.rb

--- mbsystem.rb
+++ mbsystem.rb
@@ -14,7 +14,7 @@ class Mbsystem < Formula
   depends_on "openmotif"
   depends_on "dwcaress/mbsystem/otps"
   option "without-check", "Disable build time checks (not recommended)"
-  #conflicts_with "dwcaress/mbsystem/mbsystem-beta", :because => "mbsystem and mbsystem-beta share the same commands“
+  conflicts_with 'dwcaress/mbsystem/mbsystem-beta', :because => 'mbsystem and mbsystem-beta share the same commands'
 
   def install
     args = [
