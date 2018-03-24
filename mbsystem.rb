09e3eeb0b3938017c9214082e6f1df1b6208c0c8
simon <zimon.kaizoku@gmail.com>
2018-03-24 23:23:39 +0100

    mirror of dwcaress' version

--- mbsystem.rb
+++ mbsystem.rb
@@ -1,8 +1,8 @@
 class Mbsystem < Formula
   desc "MB-System seafloor mapping software"
   homepage "http://www.mbari.org/products/research-software/mb-system/"
-  url "ftp://mbsystemftp@ftp.mbari.org/mbsystem-5.5.2309.tar.gz"
-  sha256 "7c719e660b4889675808e4c575b0bf9e98890599f776247e85eafe0224d83702"
+  url "ftp://mbsystemftp@ftp.mbari.org/mbsystem-5.5.2327.tar.gz"
+  sha256 "cd41a30b7f5030814df4058882ea5f93ff05df67ba8e2a8d2dd88cb4f78db4f8"
 
   depends_on :x11
   depends_on "gmt"
@@ -12,9 +12,9 @@ class Mbsystem < Formula
   depends_on "fftw"
   depends_on "gv"
   depends_on "openmotif"
-  depends_on "zimonkaizoku/mbsystem/otps"
-
+  depends_on "dwcaress/mbsystem/otps"
   option "without-check", "Disable build time checks (not recommended)"
+  conflicts_with 'dwcaress/mbsystem/mbsystem-beta', :because => 'mbsystem and mbsystem-beta share the same commands'
 
   def install
     args = [
@@ -27,7 +27,7 @@ class Mbsystem < Formula
       "--with-fftw-include=#{Formula["fftw"].opt_include}",
       "--with-motif-lib=#{Formula["openmotif"].opt_lib}",
       "--with-motif-include=#{Formula["openmotif"].opt_include}",
-      "--with-otps-dir=#{Formula["zimonkaizoku/mbsystem/otps"].prefix}"
+      "--with-otps-dir=#{Formula["dwcaress/mbsystem/otps"].prefix}"
     ]
 
     ENV['CFLAGS']="-I/opt/X11/include -L/opt/X11/lib"
@@ -38,7 +38,7 @@ class Mbsystem < Formula
   end
 
   def caveats
-    <<-EOS.undent
+    <<~EOS
 
       The GMT_CUSTOM_LIBS needs to be set for all users
       on this computer that want to use mbsystem. Run the
